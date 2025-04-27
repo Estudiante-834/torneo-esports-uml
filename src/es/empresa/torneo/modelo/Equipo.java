03e98a5a203606338b794817ef51b5e3a1cf5a03
Estudiante-834 <Estudiante_834@outlook.com>
2025-04-27 23:10:26 +0200

    ADD: Clases modelo con atributos y métodos

--- src/es/empresa/torneo/modelo/Equipo.java
+++ src/es/empresa/torneo/modelo/Equipo.java
@@ -1,5 +1,78 @@
 package es.empresa.torneo.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Equipo {
+	private String idEquipo;
+    private String nombre;
+    private List<Jugador> listaJugadores = new ArrayList<>();
+    private int rankingElo;
+    private String participacion;
+	
+    public Equipo(String idEquipo, String nombre, List<Jugador> listaJugadores, int rankingElo, String participacion) {
+		super();
+		this.idEquipo = idEquipo;
+		this.nombre = nombre;
+		this.listaJugadores = listaJugadores;
+		this.rankingElo = rankingElo;
+		this.participacion = participacion;
+	}
+
+	public Equipo() {
+		super();
+	}
+
+	public String getIdEquipo() {
+		return idEquipo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public List<Jugador> getListaJugadores() {
+		return listaJugadores;
+	}
+
+	public int getRankingElo() {
+		return rankingElo;
+	}
+
+	public void setRankingElo(int rankingElo) {
+		this.rankingElo = rankingElo;
+	}
+
+	public void setParticipacion(String participacion) {
+		this.participacion = participacion;
+	}
+
+	@Override
+	public String toString() {
+		return "Equipo [idEquipo=" + idEquipo + ", nombre=" + nombre + ", listaJugadores=" + listaJugadores
+				+ ", rankingElo=" + rankingElo + ", participacion=" + participacion + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(idEquipo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Equipo other = (Equipo) obj;
+		return Objects.equals(idEquipo, other.idEquipo);
+	}
 
 }
--- src/es/empresa/torneo/modelo/Jugador.java
+++ src/es/empresa/torneo/modelo/Jugador.java
@@ -1,5 +1,80 @@
 package es.empresa.torneo.modelo;
 
+import java.util.Objects;
+
 public class Jugador {
+	private String idJugador;
+    private String nombre;
+    private String apodo;
+    private int rankingElo;
+    private String participacion;
+	
+    public Jugador(String idJugador, String nombre, String apodo, int rankingElo, String participacion) {
+		super();
+		this.idJugador = idJugador;
+		this.nombre = nombre;
+		this.apodo = apodo;
+		this.rankingElo = rankingElo;
+		this.participacion = participacion;
+	}
+
+	public Jugador() {
+		super();
+	}
+
+	public String getIdJugador() {
+		return idJugador;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApodo() {
+		return apodo;
+	}
+
+	public void setApodo(String apodo) {
+		this.apodo = apodo;
+	}
+
+	public int getRankingElo() {
+		return rankingElo;
+	}
+
+	public void setRankingElo(int rankingElo) {
+		this.rankingElo = rankingElo;
+	}
+
+	public void setParticipacion(String participacion) {
+		this.participacion = participacion;
+	}
+
+	@Override
+	public String toString() {
+		return "Jugador [idJugador=" + idJugador + ", nombre=" + nombre + ", apodo=" + apodo + ", rankingElo="
+				+ rankingElo + ", participacion=" + participacion + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(idJugador);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Jugador other = (Jugador) obj;
+		return Objects.equals(idJugador, other.idJugador);
+	}
 
 }
